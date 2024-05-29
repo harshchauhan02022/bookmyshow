9b44c2842397e57aecc4d36af2eeeb61c9a658f3
Mohit Kumar <mohit2991kumar@gmail.com>
2024-05-29 11:56:50 +0530

    add the files

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -10,17 +10,18 @@ const CarouselComponent = () => {
  };
 
  const handlePrev = () => {
-  setIndex((prevIndex) => (prevIndex === 0 ? 3 : prevIndex - 1));
+  setIndex((prevIndex) => (prevIndex === 0 ? 4 : prevIndex - 1));
  };
 
  const handleNext = () => {
-  setIndex((prevIndex) => (prevIndex === 3 ? 0 : prevIndex + 1));
+  setIndex((prevIndex) => (prevIndex === 4 ? 0 : prevIndex + 1));
  };
 
  const Banner1 = "/banner/main-banner1.png";
  const Banner2 = "/banner/main-banner2.png";
  const Banner3 = "/banner/main-banner3.png";
  const Banner4 = "/banner/main-banner4.png";
+ const Banner5 = "/banner/main-banner5.png";
 
  return (
   <div className="text-center mt-3 position-relative">
@@ -37,6 +38,9 @@ const CarouselComponent = () => {
     <BootstrapCarousel.Item>
      <img className="d-block w-100" src={Banner4} alt="Fourth slide" />
     </BootstrapCarousel.Item>
+    <BootstrapCarousel.Item>
+     <img className="d-block w-100" src={Banner5} alt="Fourth slide" />
+    </BootstrapCarousel.Item>
    </BootstrapCarousel>
    <button className="carousel-control-prev custom-prev" onClick={handlePrev}>
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -47,7 +51,7 @@ const CarouselComponent = () => {
     <span className="sr-only">Next</span>
    </button>
    <ol className="carousel-indicators custom-indicators">
-    {[0, 1, 2, 3].map((indicatorIndex) => (
+    {[0, 1, 2, 3, 4].map((indicatorIndex) => (
      <li
       key={indicatorIndex}
       data-target="#carouselExampleIndicators"
--- src/screens/Events.jsx
+++ src/screens/Events.jsx
@@ -1,10 +1,63 @@
+import React, { useState } from 'react';
 
-import React, { useContext } from "react";
- import ThemeContext from "../context/ThemeContext";
+const Events = () => {
+ const [showDateDropdown, setShowDateDropdown] = useState(false);
+ const [showLanguagesDropdown, setShowLanguagesDropdown] = useState(false);
 
-const  Event = () => {
- const { textColor } = useContext(ThemeContext);
- return <div className={textColor}>Event</div>;
-};
+ const toggleDateDropdown = () => {
+  setShowDateDropdown(!showDateDropdown);
+ };
 
-export default Event;
+ const toggleLanguagesDropdown = () => {
+  setShowLanguagesDropdown(!showLanguagesDropdown);
+ };
+
+ const redTextStyle = {
+  color: 'red',
+ };
+
+ return (
+  <div className="d-flex align-items-center justify-content-center">
+   <div className="container-fluid">
+    <div className="row">
+     <div className="col-md-2">
+      <h3>Filters</h3>
+      <div className="Filters">
+       <div onClick={toggleDateDropdown}>
+        <span style={{ marginRight: '20px' }}>˅</span>
+        <span>Date</span>
+
+        {showDateDropdown && (
+         <div style={redTextStyle}>
+          <div>Today</div>
+          <div>Tomorrow</div>
+          <div>This week</div>
+         </div>
+        )}
+       </div>
+      </div>
+
+      <div>
+       <div onClick={toggleLanguagesDropdown}>
+        <span style={{ marginRight: '20px' }}>˅</span>
+        <span>Languages</span>
+       </div>
+       {showLanguagesDropdown && (
+        <div style={redTextStyle}>
+         <div>English</div>
+         <div>Hindi</div>
+         <div>Punjabi</div>
+        </div>
+       )}
+      </div>
+     </div>
+     <div className="col-md-6">
+      <h3>Event In Agra</h3>
+     </div>
+    </div>
+   </div>
+  </div>
+ );
+}
+
+export default Events;
